d72f04dc5009f591233b3b93f7a6932dd5d80a51
Noushin <nou.babakhani@gmail.com>
2024-06-26 18:29:36 +0100

    DriverManager Added

--- src/test/java/core/drivers/DriverManager.java
+++ src/test/java/core/drivers/DriverManager.java
@@ -1,5 +1,20 @@
 package core.drivers;
 
-public class DriverManager {
+import io.appium.java_client.AppiumDriver;
 
+public class DriverManager {
+private static final ThreadLocal<AppiumDriver> driverThread=new ThreadLocal<>();
+public static AppiumDriver getDriver(){
+    return driverThread.get();
+}
+public static void setDriver(AppiumDriver appiumDriver){
+    driverThread.set(appiumDriver);
+}
+public static void removeDriver(){
+    AppiumDriver driver=getDriver();
+    if (driver!=null){
+        driver.quit();
+        driverThread.remove();
+    }
+}
 }
