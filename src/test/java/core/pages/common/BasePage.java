11317b4b41279fb09e420dcd07772447c56f6bee
Noushin <nou.babakhani@gmail.com>
2024-06-29 15:32:11 +0100

    BasePage improved

--- src/test/java/core/pages/common/BasePage.java
+++ src/test/java/core/pages/common/BasePage.java
@@ -2,6 +2,7 @@ package core.pages.common;
 
 import core.drivers.DriverManager;
 import core.library.ReusableMethods;
+import core.library.ReusableUIMethods;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.support.PageFactory;
@@ -10,11 +11,13 @@ public class BasePage {
 
     protected AppiumDriver driver;
     protected ReusableMethods reusableMethods;
+    protected ReusableUIMethods reusableUIMethods;
 
     public BasePage(){
         this.driver= DriverManager.getDriver();
         PageFactory.initElements(new AppiumFieldDecorator(driver),this);
         reusableMethods=new ReusableMethods();
+        reusableUIMethods=new ReusableUIMethods(driver);
     }
 
 }
