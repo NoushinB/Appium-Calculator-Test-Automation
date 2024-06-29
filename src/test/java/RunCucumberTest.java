30a7c5acc2a52a2940fcaa5420649e8453860d9e
Noushin <nou.babakhani@gmail.com>
2024-06-29 15:10:16 +0100

    RunCucumberTest Added

---
+++ src/test/java/RunCucumberTest.java
@@ -0,0 +1,13 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+features = "src/test/resources/features",
+        publish = true,
+        plugin = "pretty",
+        tags = "not @todo"
+)
+public class RunCucumberTest {
+}
